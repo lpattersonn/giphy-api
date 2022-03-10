a322bf5d7f4ec4d25848f03bd9c1267dc1b6162b
Leshan <leshan0627@gmail.com>
2022-03-10 00:10:20 -0500

    chore: Setup route for API call from GIPHY

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-const db = "api.giphy.com/v1/gifs/trending"
-
-var app = express();
+const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(cors())
 app.use(logger('dev'));
@@ -23,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter(db));
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
-const axios = require('axios')
-/* GET home page. */
-axios.get('api.giphy.com/v1/gifs/trending');
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+require("dotenv/config")
+
+/*  */
+router.get("/", function (req, res, next) {
+  axios
+    .get(
+      `https://api.giphy.com/v1/gifs/search?api_key=${process.env.api_key}&q=spongebob`
+    )
+    .then((response) => {
+      console.log(response.data);
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 module.exports = router;
